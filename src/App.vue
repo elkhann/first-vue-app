76b131b81920357319fad481cb668f16ddd9f266
elkhann <emilkhann@gmail.com>
2018-06-05 01:25:10 +0300

    Создание простых фильтров

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h2 v-colored:background.font="'red'" v-colored:color="'white'">{{ title }}</h2>
-    <h2 v-colored:color.delay.font="'blue'">{{ title }}</h2>
-
-    <h2 v-font>{{ title2 }}</h2>
+    <h2>{{ title }}</h2>
+    <h2>{{ title | lowercase }}</h2>
+    <h2>{{ title | uppercase }}</h2>
+    <h2>{{ title | uppercase | lowercase }}</h2>
   </div>
 </template>
 
@@ -12,18 +12,13 @@
 export default {
   data() {
     return {
-      title: 'Hello, I\'m Vue',
-      title2: 'Local font directive',
-      visible: true
+      title: 'Hello I\'m Vue'
     }
   },
-  directives: {
-    font: {
-      bind(el, bindings, vnode) {
-        el.style.fontSize = '40px'
-      }
+  filters: {
+    lowercase(value) {
+      return value.toLowerCase()
     }
-    
   }
 }
 
