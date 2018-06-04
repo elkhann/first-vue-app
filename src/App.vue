ac6884e3a72ab13cca2a47ff1998b120b91760e6
elkhann <emilkhann@gmail.com>
2018-06-04 16:27:53 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <h1>{{ msg }}</h1>
+
+    <app-car></app-car>
+
+  </div>
+</template>
+
+<script>
+import Car from './Car.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+
+  },
+  components: {
+    appCar: Car
+  }
+}
+
+</script>
+
+<style>
+
+</style>
