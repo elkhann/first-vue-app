84df21ab92b12e4b4fe6938ddde2b79338d6060b
elkhann <emilkhann@gmail.com>
2018-06-05 01:06:41 +0300

    Локальная регистрация

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div>
     <h2 v-colored:background.font="'red'" v-colored:color="'white'">{{ title }}</h2>
     <h2 v-colored:color.delay.font="'blue'">{{ title }}</h2>
+
+    <h2 v-font>{{ title2 }}</h2>
   </div>
 </template>
 
@@ -11,8 +13,17 @@ export default {
   data() {
     return {
       title: 'Hello, I\'m Vue',
+      title2: 'Local font directive',
       visible: true
     }
+  },
+  directives: {
+    font: {
+      bind(el, bindings, vnode) {
+        el.style.fontSize = '40px'
+      }
+    }
+    
   }
 }
 
