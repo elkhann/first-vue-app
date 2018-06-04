362001fef62b839f5a8caa07ffef9cfd317a2e49
elkhann <emilkhann@gmail.com>
2018-06-04 17:22:17 +0300

    Валидация входящих параметров

--- src/Car.vue
+++ src/Car.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="car">
-    <h3>Name: {{ carName }}</h3>
+    <h3>Name: {{ carName }} | {{ reverseCarName }}</h3>
     <p>Year: {{ carYear }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['carName','carYear']
+  // props: ['carName','carYear'],
+  // props: {
+  //   carName: String,
+  //   carYear: Number
+  // },
+  props: {
+    carName: {
+      type: String,
+      // required: true,
+      default: 'Default auto name' 
+    },
+    carYear: Number
+  },
+  computed: {
+    reverseCarName() {
+      return this.carName.split('').reverse().join('')
+    }
+  }
 }
 </script>
 
