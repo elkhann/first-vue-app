d2f266489295368f68a7dd93b96f9b9d819858d7
elkhann <emilkhann@gmail.com>
2018-06-05 01:00:47 +0300

    Модификаторы

--- src/color.js
+++ src/color.js
@@ -1,7 +1,20 @@
 export default {
   bind(el, bindings, vnode) {
-    // el.style.color = 'blue'
-    const arg = bindings.arg
-    el.style[arg] = bindings.value
+
+    const fondModifier = bindings.modifiers['font']
+    if (fondModifier) {
+      el.style.fontSize = '30px'
+    }
+
+    const delayModifier = bindings.modifiers['delay']
+    let delay = 0    
+    if (delayModifier) { // если есть delay, то
+      delay = 2000 //2 сек.
+    }
+
+    setTimeout(() =>{
+      const arg = bindings.arg //[background="'red'", color="'white'", color="'blue'"]
+      el.style[arg] = bindings.value //el.style.background = 'red'
+    }, delay)
   }
 }
